dd0686bfe05440bbb9072b2dc0d0de3943427b36
ravenel <ravenel@narsil.(none)>
2012-06-25 22:49:09 -0400

    Added retries to twitter API and fixed misc bugs.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from __future__ import division #WTF, python. Really?
 import requests
 import csv
 import time
@@ -7,18 +8,14 @@ import time
 #   Config Settings
 #
 ##################################################
-#URL of listing of top users. Page num will be appended.
-root_url = 'http://www.behance.net/?content=users&sort=views&time=all&page='
-#Root user of users. User name will be appended.
-user_page_root_url = r'http://www.behance.net'
-#Number of pages to check
-number_of_pages = 2
-#Page number to start on
-start_page = 1
 #File location to place output
 outfile = r'/home/ravenel/code/BehanceTopUserFollowers/output.csv'
 #Location of csv file with behance names, twitter handles, views
 src = r'users.csv'
+#Rate limit per hour for twitter
+rate_limit = 150
+#Dont change this
+_rate_per_second = (rate_limit - 5) / 60 / 60
 
 def chunks(l, n=100):
     """Yield n-sized chunks from list l"""
@@ -40,7 +37,7 @@ def RateLimited(maxPerSecond):
             elapsed = time.clock() - lastTimeCalled[0]
             leftToWait = minInterval - elapsed
             if leftToWait > 0:
-                print "Rate limited: sleep for %s seconds..." % leftToWait
+                print "Rate limited: sleep for %s seconds..." % leftToWait,
                 time.sleep(leftToWait)
             ret = func(*args, **kwargs)
             lastTimeCalled[0] = time.clock()
@@ -53,29 +50,34 @@ def _scrub_twitter_handle(handle):
     if handle == '-' or handle == '#!' or handle == '':
         return None
     handle.replace('@', '').replace('#!', '').replace('/', '')
-    return handle
+    return handle.strip()
 
 #Rate limit to 150 requests/hour for Twitter
-@RateLimited(0.04)
-def _get_twitter_followers_chunked(handle_list):
+@RateLimited(_rate_per_second)
+def _get_twitter_followers_chunked(handle_list, retries=3):
     """Get number of twitter followers for a given list of handles. Makes request
     in chunks of 100 to conserve API calls. 
     Return a dict with key: handle, value: followers"""
-    print "Getting twitter data for 100 users...",
     twitter_url = r'https://api.twitter.com/1/users/lookup.json?include_entities=true&screen_name='
     handle_text = ",".join(handle_list)
     return_dict = {}
+    retries_left = retries
 
-    t = requests.post(twitter_url + handle_text)
-    if t.status_code == 200:
-        #Update output list
-        for user_json in t.json:
-            return_dict[user_json['screen_name']] = user_json['followers_count']
-    else:
-        #Update with errors
-        return_dict = dict((k, "Twitter Error: %s" % t.status_code) for k in handle_list)
-
-    print "Done."
+    while retries_left > 0:
+        t = requests.post(twitter_url + handle_text)
+        if t.status_code == 200:
+            #Update output list
+            for user_json in t.json:
+                return_dict[user_json['screen_name']] = user_json['followers_count']
+            retries_left = 0
+        else:
+            #Retry
+            retries_left -= 1
+            print "Twitter returned status of %s. %s more retries..." % (t.status_code, retries_left)
+            time.sleep(1 / _rate_per_second)
+            if retries_left ==  0:
+                #Update with errors
+                return_dict = dict((k, "Twitter Error: %s" % t.status_code) for k in handle_list)
 
     return return_dict
 
@@ -104,11 +106,13 @@ def parse_from_csv(csv_location):
 
     #Parse list of Behance users and twitter handles
     with open(csv_location, 'rb') as f:
+        print "Parsing csv of user data...",
         reader = csv.reader(f)
         for row in reader:
             users[row[0]] = {}
             users[row[0]]['twitter_handle'] = _scrub_twitter_handle(row[1])
             users[row[0]]['behance_views'] = row[2]
+        print "Done."
 
     with open(outfile, 'wb') as of:
         #Setup output csv
@@ -120,10 +124,11 @@ def parse_from_csv(csv_location):
         #Walk through the csv values
         for chunk in chunk_dict(users):
 
+            print "Parsing 100 users...",
+
             #Update chunk with twitter follower counts
             twitterfied_chunk = _twitterfy_chunk(chunk)
 
-            print "Writing chunk to csv...",
             for user, user_data in twitterfied_chunk.items():
 
                 outrow = {}
